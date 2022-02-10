eb018ba7a2964bc23d0b95c16c879e270f9a6d35
Ruslan Kadyrov <kadyrov7498@gmail.com>
2022-02-10 21:24:41 +0300

    Initial commit

---
+++ src/main/java/repository/PostRepository.java
@@ -0,0 +1,40 @@
+package repository;
+
+import model.Post;
+
+import java.io.IOException;
+import java.util.*;
+
+// Stub
+public class PostRepository {
+    private final Map<Long, Post> mapPosts = new HashMap<>();
+    private long id = 1;
+
+    public List<Post> all() {
+        return new ArrayList<>(mapPosts.values());
+    }
+
+    public Optional<Post> getById(long id) {
+        return Optional.ofNullable(mapPosts.get(id));
+    }
+
+    public synchronized Post save(Post post) throws IOException {
+        if (post.getId() == 0) {
+            Post newPost = new Post(id, post.getContent());
+            mapPosts.put(id, newPost);
+            id++;
+            return newPost;
+        }
+        if (mapPosts.containsKey(post.getId())) {
+            Post replacePost = new Post(post.getId(), post.getContent());
+            mapPosts.put(post.getId(), replacePost);
+            return replacePost;
+        } else {
+            throw new IOException("Error id");
+        }
+    }
+
+    public synchronized void removeById(long id) {
+        mapPosts.remove(id);
+    }
+}
---
+++ src/main/java/servlet/MainServlet.java
@@ -0,0 +1,78 @@
+package servlet;
+
+import controller.PostController;
+import repository.PostRepository;
+import service.PostService;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MainServlet extends HttpServlet {
+    private PostController controller;
+    private final String GET = "GET";
+    private final String POST = "POST";
+    private final String DELETE = "DELETE";
+    private final String DELIMITER = "/";
+    private final String API_POSTS = "/api/posts";
+    private final String API_POSTS_D = "/api/posts\\d+";
+
+    @Override
+    public void init() {
+        final PostRepository repository = new PostRepository();
+        final PostService service = new PostService(repository);
+        controller = new PostController(service);
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        //resp.getWriter().print("Ok");
+        //resp.setStatus(HttpServletResponse.SC_OK);
+        // если деплоились в root context, то достаточно этого
+        try {
+            final String path = req.getRequestURI();
+            final String method = req.getMethod();
+            // primitive routing
+            if (method.equals(GET)) {
+                methodGet(resp, path);
+                return;
+            }
+            if (method.equals(POST)) {
+                methodPost(req, resp, path);
+                return;
+            }
+            if (method.equals(DELETE)) {
+                methodDelete(resp, path);
+                return;
+            }
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public void methodGet(HttpServletResponse resp, String path) throws IOException {
+        if (path.equals(API_POSTS)) {
+            controller.all(resp);
+        }
+        if (path.matches(API_POSTS_D)) {
+            final long id = Long.parseLong(path.substring(path.lastIndexOf(DELIMITER)));
+            controller.getById(id, resp);
+        }
+    }
+
+    public void methodPost(HttpServletRequest req, HttpServletResponse resp, String path) throws IOException {
+        if (path.equals(API_POSTS)) {
+            controller.save(req.getReader(), resp);
+        }
+    }
+
+    public void methodDelete(HttpServletResponse resp, String path) throws IOException {
+        if (path.matches(API_POSTS_D)) {
+            final long id = Long.parseLong(path.substring(path.lastIndexOf(DELIMITER)));
+            controller.removeById(id, resp);
+        }
+    }
+}
